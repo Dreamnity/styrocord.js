8ea996f4f8c3297dafc171e1faccec09edc85926
superdinmc <superdinmc@gmail.com>
2023-09-05 13:09:14 +0700

    - Moved login to login function - Promisified and moved this,socket.send to function - Resume handling

--- index.js
+++ index.js
@@ -46,60 +46,7 @@ class Styrofoam extends EventEmitter {
 			});
 		download(join(APIs.https, 'gateway')).then(result => {
 			APIs.gateway = JSON.parse(result).url;
-			//console.log('Logging in...');
-			this.socket = new ws(APIs.gateway).on('message', r => {
-				let socket = this.socket;
-				let data = JSON.parse(r),
-					heartbeat;
-				//console.log(data);
-				switch (data.op) {
-					case 10:
-						this.#heartbeat = data.d.heartbeat_interval;
-						socket.send(
-							JSON.stringify({
-								op: 2,
-								d: {
-									properties: {
-										os: process.platform,
-										browser: 'dx.js',
-										device: process.arch,
-									},
-									intents: 513,
-									...options.login,
-								},
-							})
-						);
-						return (heartbeat = () =>
-							setTimeout(() => {
-								if (this.#heartbeat == 0) return;
-								if (this.#pong) {
-									this.emit('ping');
-									this.#pong = false;
-									heartbeat();
-									this.#pingtime = Date.now();
-									socket.send(JSON.stringify({ op: 1, d: this.#lastevent }));
-								} else {
-									socket.close();
-									this.emit('close', 'Timed out!');
-									this.#heartbeat = 0;
-								}
-							}, this.#heartbeat))();
-					case 11:
-						this.#pong = true;
-						this.ping = Date.now() - this.#pingtime;
-						return this.emit('pong');
-					case 1:
-					case 0:
-						this.emit(toCamelCase(data.t.replace(/_/g, ' ')), data.d);
-				}
-				this.#lastevent = data.s || this.#lastevent;
-			});
-			this.socket.on('close', data => {
-				if (data == 1005) return;
-				this.emit('close', data);
-				this.#heartbeat = 0;
-			});
-			this.#destroy = EventEmitter.destroy;
+			this.login();
 		});
 	}
 	#validateLogin(options) {
@@ -107,6 +54,7 @@ class Styrofoam extends EventEmitter {
 			throw new ReferenceError('Token is not provided! (options.login.token)');
 		this.#token = options.login.token;
 	}
+	session = {};
 	#token = '';
 	#heartbeat = 0;
 	#pingtime = 0;
@@ -148,6 +96,91 @@ class Styrofoam extends EventEmitter {
 		});
 	}
 	/**
+	 * Login to the bot
+	 * @param {Object} option Login option
+	 */
+	login({ gateway,customLogin }) {
+		if(this.socket) this.socket.close();
+		this.socket = new ws(gateway || APIs.gateway).on('message', r => {
+			let socket = this.socket;
+			let data = JSON.parse(r),
+				heartbeat;
+			//console.log(data);
+			switch (data.op) {
+				case 10:
+					this.#heartbeat = data.d.heartbeat_interval;
+					socket.send(
+						JSON.stringify(customLogin||{
+							op: 2,
+							d: {
+								properties: {
+									os: process.platform,
+									browser: 'dx.js',
+									device: process.arch,
+								},
+								intents: 513,
+								...options.login,
+							},
+						})
+					);
+					return (heartbeat = () => {
+						const timeout = setTimeout(() => {
+							if (this.#heartbeat == 0) return;
+							if (this.#pong) {
+								this.emit('ping');
+								this.#pong = false;
+								heartbeat();
+								this.#pingtime = Date.now();
+								socket.send(JSON.stringify({ op: 1, d: this.#lastevent }));
+							} else {
+								socket.close();
+								this.emit('close', 'Timed out!');
+								this.#heartbeat = 0;
+							}
+						}, this.#heartbeat);
+						this.on('close', () => clearTimeout(timeout));
+					})();
+				case 11:
+					this.#pong = true;
+					this.ping = Date.now() - this.#pingtime;
+					return this.emit('pong');
+				case 1:
+					if(data.d) this.session = data.d;
+				case 0:
+					return this.emit(toCamelCase(data.t.replace(/_/g, ' ')), data.d);
+				case 7:
+				case 9:
+					this.emit('disconnected', data.d);
+					if (data.d || data.op == 7) {
+						this.login({
+							customLogin: {
+								op: 6,
+								d: {
+									token: this.#token,
+									session_id: this.session.session_id,
+									seq: this.#lastevent,
+								},
+							},gateway:this.session.resume_gateway_url
+						})
+					} else {
+						this.emit(
+							'error',
+							new Error('Gateway issued disconnection without resume')
+						);
+						this.emit('close', 'Connection closed by gateway!');
+						this.destroy();
+					}
+			}
+			this.#lastevent = data.s || this.#lastevent;
+		});
+		this.socket.on('close', data => {
+			if (data == 1005) return;
+			this.emit('close', data);
+			this.#heartbeat = 0;
+		});
+		this.#destroy = EventEmitter.destroy;
+	}
+	/**
 	 * Log out the bot
 	 */
 	destroy() {
@@ -161,7 +194,7 @@ class Styrofoam extends EventEmitter {
 		}
 	}
 	/**
-	 * Fr
+	 * Set activity of the bot
 	 * @param {Object} activity Activity information
 	 * @param {String} activity.name Activity's name
 	 * @param {Number} activity.type https://discord.com/developers/docs/topics/gateway-events#activity-object-activity-types
@@ -169,38 +202,44 @@ class Styrofoam extends EventEmitter {
 	 * @param {String|undefined} activity.url Stream URL, is validated when type is 1
 	 */
 	setPresence(activity) {
-		this.#currentPresense.activity=activity;
-		this.socket.send(
-			JSON.stringify({
-				op: 3,
-				d: {
-					since: null,
-					activities: [activity],
-					status: this.#currentPresense.status,
-					afk: false,
-				},
-			})
+		this.#currentPresense.activity = activity;
+		return this.#send({
+					op: 3,
+					d: {
+						since: null,
+						activities: [activity],
+						status: this.#currentPresense.status,
+						afk: false,
+					},
+				})
+	}
+	#send(data) {
+		return new Promise(r =>
+			this.socket.send(
+				typeof data==='string'?data:JSON.stringify(data),
+				e => (e ? j(e) : r())
+			)
 		);
 	}
 	/**
-	 * 
-	 * @param {'online'|'dnd'|'idle'|'invisible'|'offline'} status 
+	 * Set the status of the bot
+	 * @param {'online'|'dnd'|'idle'|'invisible'|'offline'} status
 	 */
 	setStatus(status) {
-		let validator = ['online','dnd','idle','invisible','offline'];
-		if (!validator.includes(status)) throw new TypeError('Status must be one of ' + validator.join(', '));
-		this.#currentPresense.status=status;
-		this.socket.send(
-			JSON.stringify({
-				op: 3,
-				d: {
-					since: null,
-					activities: [this.#currentPresense.activity],
-					status: status,
-					afk: false,
-				},
-			})
-		);
+		let validator = ['online', 'dnd', 'idle', 'invisible', 'offline'];
+		if (!validator.includes(status))
+			throw new TypeError('Status must be one of ' + validator.join(', '));
+		this.#currentPresense.status = status;
+		return this.#send({
+					op: 3,
+					d: {
+						since: null,
+						activities: [this.#currentPresense.activity],
+						status: status,
+						afk: false,
+					},
+				}
+			)
 	}
 	#currentPresense = { status: 'online', activity: {} };
 	//EventEmitter's destroy
