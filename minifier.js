0743eae571a60351ae3c137674c86f3b6d3df46e
5UP3R_D1N <superdinmc@gmail.com>
2024-05-08 05:09:46 +0000

    Yeet ws.js out

--- minifier.js
+++ minifier.js
@@ -1,2 +1,23 @@
 const { writeFileSync, readFileSync } = require('fs')
-fetch(new URL('/developers/javascript-minifier/api/raw', 'https://www.toptal.com'), { method: 'POST',headers:{'Content-Type':'application/x-www-form-urlencoded'},body:'input='+encodeURIComponent(readFileSync('index.js').toString()) }).then(e => e.text()).then(e => writeFileSync('minified.js', e));
+const ws = readFileSync("./ws.js").toString();
+fetch(
+	new URL("/developers/javascript-minifier/api/raw", "https://www.toptal.com"),
+	{
+		method: "POST",
+		headers: { "Content-Type": "application/x-www-form-urlencoded" },
+		body:
+			"input=" +
+			encodeURIComponent(
+				readFileSync("index.js")
+					.toString()
+					.replace(
+						"require('./ws')",
+						ws
+							.substring(1, ws.length - 1)
+							.replace("module.exports=pe", "return pe")
+					)
+			),
+	}
+)
+	.then(e => e.text())
+	.then(e => writeFileSync("minified.js", e));
